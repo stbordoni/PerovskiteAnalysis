eda45eaff018098b1a91de01c9c6fd9658124f2e
Stefania <stbordoni@gmail.com>
2025-07-08 14:02:31 +0200

    updated macros for plots on NPhotons

--- PeroAna/macros/compareThreeRuns.C
+++ PeroAna/macros/compareThreeRuns.C
@@ -84,22 +84,26 @@ void compareThreeRuns(TString f_1 ="run1",
     if (tree3_ok) tree3->Draw("peakInt>>htemp3int(100, 0, 0.5)", "", "sames");
     leg->Draw("same");
 
-    TCanvas *c2 = new TCanvas("c2", " ", 1600, 600);
+    TCanvas *c2 = new TCanvas("c2", " tail integral - npeaks", 1600, 600);
     c2->Divide(2,1);
     c2->cd(1);
-    if (tree1_ok) tree1->Draw("tailInt>>htemp1tail(100, 0, 10)");     
-    //htemp1->GetXaxis()->SetTitle("[V]");
+    
+    if (tree1_ok) tree1->Draw("tailInt/0.09636>>htemp1tail(100, 0, 100)");     
+    //htemp1tail->GetXaxis()->SetTitle("Nb of Photons");
+    //htemp1tail->GetYaxis()->SetTitle("Entries");
     //htemp1->GetYaxis()->SetTitle("counts");
-    if (tree2_ok) tree2->Draw("tailInt>>htemp2tail(100, 0, 10)", "", "sames");
-    if (tree3_ok) tree3->Draw("tailInt>>htemp3tail(100, 0, 10)", "", "sames");
+    if (tree2_ok) tree2->Draw("tailInt/0.09636>>htemp2tail(100, 0, 100)", "", "sames");
+    if (tree3_ok) tree3->Draw("tailInt/0.09636>>htemp3tail(100, 0, 100)", "", "sames");
     leg->Draw("same");
     
     c2->cd(2);
-    if (tree1_ok) tree1->Draw("npeaks>>htemp1npeaks(12, -0.5, 11.5)");     
-    //htemp1->GetXaxis()->SetTitle("[V]");
-    //htemp1->GetYaxis()->SetTitle("counts");
-    if (tree2_ok) tree2->Draw("npeaks>>htemp2npeaks(12, -0.5, 11.5)", "", "sames");
-    if (tree3_ok) tree3->Draw("npeaks>>htemp3npeaks(12, -0.5, 11.5)", "", "sames");
+    //TH1F *htemp1npeaks;
+
+    if (tree1_ok) tree1->Draw("npeaks_specut>>htemp1npeaks(12, -0.5, 11.5)");     
+    //htemp1npeaks->GetXaxis()->SetTitle("Nb of Peaks");
+    //htemp1npeaks->GetYaxis()->SetTitle("Entries");
+    if (tree2_ok) tree2->Draw("npeaks_specut>>htemp2npeaks(12, -0.5, 11.5)", "", "sames");
+    if (tree3_ok) tree3->Draw("npeaks_specut>>htemp3npeaks(12, -0.5, 11.5)", "", "sames");
 
     TCanvas *c3 = new TCanvas("c3", "Baseline", 800, 600);
     c3->cd();
@@ -115,23 +119,23 @@ void compareThreeRuns(TString f_1 ="run1",
     c4->Divide(2,1);
     c4->cd(1);
     if (tree1_ok){
-        TH2F* htemp1tailnPeaks = (TH2F*)tree1->Draw("tailInt:npeaks>>htemp1tailnPeaks(14, 0, 14, 50, -1, 10)", "", "COLZ");
+        TH2F* htemp1tailnPeaks = (TH2F*)tree1->Draw("tailInt/0.09636:npeaks>>htemp1tailnPeaks(14, 0, 14, 100, 0, 100)", "", "COLZ");
     //hist2d->Draw("COLZ");
         //if (tree1_ok) tree1->Draw("tailInt:npeaks", "", "COLZ");     
         //htemp1tailnPeaks->GetXaxis()->SetTitle("N peaks");
-        //htemp1tailnPeaks->GetYaxis()->SetTitle("tail integral [V]");
+        //htemp1tailnPeaks->GetYaxis()->SetTitle("Nb of Photons");
     }
         //if (tree3_ok) tree3->Draw("tailInt:npeaks>>htemp3tailnPeaks(100, 0, 10, 12, -0.5, 11.5)", "", "sames");
     //leg->Draw("same");
     
     c4->cd(2);
     if (tree2_ok){
-    TH2F* htemp2tailnPeaks = (TH2F*)tree2->Draw("tailInt:npeaks>>htemp2tailnPeaks( 14, 0, 14, 50, -1, 10)", "", "COLZ");
+    TH2F* htemp2tailnPeaks = (TH2F*)tree2->Draw("tailInt/0.09636:npeaks>>htemp2tailnPeaks( 14, 0, 14, 100, 0, 100)", "", "COLZ");
     //if (tree2_ok) tree2->Draw("(tailInt:npeaks)>>htemp2tailnPeaks(100, 0, 10, 12, -0.5, 11.5)", "", "");
     
     //if (tree2_ok) tree2->Draw("tailInt:npeaks", "", "COLZ");
     //htemp2tailnPeaks->GetXaxis()->SetTitle("N peaks");
-    //htemp2tailnPeaks->GetYaxis()->SetTitle("tail integral [V]");
+    //htemp2tailnPeaks->GetYaxis()->SetTitle("Nb of Photons ");
     }
     
 
